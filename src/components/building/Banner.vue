546fbf6a77bdf51d7698b14ccb06fcc5b45aa8a2
Rosa Amelia Ocaña Bermudez <lavieenrouse95@gmail.com>
2019-09-01 23:43:43 -0700

    Add Banner to building view

--- src/components/building/Banner.vue
+++ src/components/building/Banner.vue
@@ -1,12 +1,41 @@
 <template>
-    <v-row>
-        <v-col>
+    <div style="width: 100%">
+        <v-row no-gutters>
+            <v-col cols="7">
+                <v-img :src="require('@/assets/building/banner.png')">
 
-        </v-col>
-        <v-col>
+                </v-img>
+            </v-col>
+            <v-col cols="5" style="position: relative">
+                <v-img :src="require('@/assets/building/banner2.png')">
 
-        </v-col>
-    </v-row>
+                </v-img>
+                <v-row no-gutters style="position: absolute; top: 0; width: 100%; height: 100%" align="end">
+                    <v-col cols="6">
+                        <div style="background-color: #eeeeed; width: 100%; height: 700px;">
+
+                        </div>
+                    </v-col>
+                    <v-col cols="6" align="end">
+                           <div>
+                               <div style="margin-left: -90%;">
+                                   <h1 class="text-left" style="white-space: nowrap; font-size: 50px;font-weight: bold; text-transform: uppercase; letter-spacing: 3px">La apuesta</h1>
+                                   <h1 class="text-left" style="white-space: nowrap;font-size: 50px;font-weight: bold; text-transform: uppercase; letter-spacing: 3px">es</h1>
+                                   <h1 class="text-left" style="white-space: nowrap;font-size: 50px;font-weight: bold; text-transform: uppercase; letter-spacing: 3px">Construir</h1>
+                               </div>
+                           </div>
+                        <div style="border-bottom: 2px solid black; width: 80%; margin-left: auto">
+
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+
+
+    </div>
+
+
 </template>
 
 <script>
--- src/views/Building.vue
+++ src/views/Building.vue
@@ -1,10 +1,59 @@
 <template>
-    
+    <div>
+        <banner></banner>
+        <v-row justify="center">
+            <v-col cols="5">
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <p class="text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </v-col>
+            <v-col cols="5">
+                <v-card>
+
+                </v-card>
+                <v-card>
+
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
+
 </template>
 
 <script>
+    import Banner from '@/components/building/Banner'
     export default {
-        name: "Building"
+        name: "Building",
+        components: {Banner}
     }
 </script>
 

a76b9d76f66c11abdd14c2667a8384d8a0ab0229
Rosa Amelia Ocaña Bermudez <lavieenrouse95@gmail.com>
2019-09-27 00:18:12 -0700

    Modify responsive behaviour on medium screen(font-size, margin, etc)

--- src/components/home/OwnersCard.vue
+++ src/components/home/OwnersCard.vue
@@ -1,12 +1,14 @@
 <template>
     <v-row align="end" justify="start">
-        <v-col cols="1" sm="1" md="1" lg="2" xl="2" class="d-none d-sm-flex d-md-flex d-lg-flex d-xl-flex">
+        <v-col cols="1" sm="2" md=2 lg="2" xl="2"
+               class="d-none d-sm-flex d-md-flex d-lg-flex d-xl-flex">
             <div class="horizontal-line"></div>
         </v-col>
-        <v-col class="pa-8 pa-sm-3 pa-md-3 pa-lg-3 pa-xl-3" cols="12" sm="10" md="10" lg="10" xl="10">
+        <v-col cols="12" sm="10" md="10" lg="10" xl="10"
+               class="pa-8 pa-sm-3 pa-md-3 pa-lg-3 pa-xl-3">
             <v-card color="gray" flat>
-                <v-row no-gutters style="width: 100%">
-                    <v-col cols="12" sm="8" md="6" lg="5">
+                <v-row no-gutters>
+                    <v-col cols="12" sm="5" md="5" lg="5" xl="5">
                         <v-card-title class="card-title-container">
                             <v-row align="center">
                                 <v-col cols="12">
@@ -14,15 +16,14 @@
                                 </v-col>
                             </v-row>
                         </v-card-title>
-                        <v-divider
-                                style="margin-left: 16px; width: 40%; border-bottom: 4px solid var(--primary-color); border-radius: 4px"></v-divider>
+                        <v-divider class="title-divider"></v-divider>
                         <v-card-text class="text-left card-text-container">
                             <v-row>
                                 <v-col class="card-text">
                                     ...ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat...
+                                    <span class="d-none d-sm-none d-md-none d-lg-flex d-xl-flex"> quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat</span>...
                                 </v-col>
                             </v-row>
                         </v-card-text>
@@ -33,7 +34,7 @@
                         </v-card-actions>
 
                     </v-col>
-                    <v-col cols="12" sm="4" md="6" lg="6"  style="position: relative;">
+                    <v-col cols="12" sm="6" md="6" lg="6" xl="6" style="position: relative;">
                         <div class="gray-panel">
                         </div>
                         <v-img class="d-none d-sm-flex d-md-flex d-lg-flex d-xl-flex" height="100%" width="6.3in"
@@ -85,6 +86,13 @@
         font-family: Montserrat-Regular;
     }
 
+    .title-divider {
+        margin-left: 16px;
+        width: 40%;
+        border-bottom: 4px solid var(--primary-color) !important;
+        border-radius: 4px
+    }
+
     .card-text-container {
         margin-top: 20px;
     }
@@ -107,15 +115,53 @@
         right: 100%;
     }
 
-    @media screen and (max-width: 360px) {
+    @media screen and (max-width: 1264px) {
         .card-title {
-            font-size: 28.28pt;
+            font-size: 35.28pt;
+        }
+
+        .card-title-container {
+            margin-top: 0.4in;
+            margin-bottom: 0in;
+        }
+
+        .more-btn {
+            margin-top: 0;
+            font-size: 16.3pt;
         }
     }
 
-    @media screen and (max-width: 768px) {
-        .gray-panel {
-            width: 100%;
+    @media screen and (max-width: 960px) {
+        .card-title {
+            font-size: 22.28pt;
+        }
+
+        .card-title-container {
+            margin-top: 0.236in;
+            margin-bottom: 0;
+        }
+
+        .card-text-container {
+            margin-top: 0px;
+            padding-bottom: 0;
+        }
+
+        .card-text {
+            line-height: 16pt;
+            font-size: 13pt;
+        }
+
+        .more-btn {
+            margin-top: 0px;
+            margin-bottom: 30px;
+            font-size: 13.3pt;
+        }
+    }
+
+
+    @media screen and (max-width: 360px) {
+        .card-title {
+            font-size: 28.28pt;
         }
     }
 

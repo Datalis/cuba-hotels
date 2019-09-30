6a8aa6f5d3494641e4df9c95c44dcc9dd0bc3de3
Rosa Amelia Ocaña Bermudez <lavieenrouse95@gmail.com>
2019-09-30 00:42:39 -0700

    Decrease margin between components

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,14 +5,12 @@
             <v-container fluid fill-height fill-width class="pl-0 pr-0 pt-0">
                 <v-layout>
                     <v-flex xs12 style="overflow-x: hidden">
-
                         <router-view></router-view>
-
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-content>
-        <core-footer style="margin-top: 200px"/>
+        <core-footer style="margin-top: 100px"/>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,10 @@
     <div>
         <banner></banner>
         <owners style="margin-top: 3in;"></owners>
-        <building-card style="margin-top: 300px;"></building-card>
+        <building-card style="margin-top:  2in;"></building-card>
         <third-card style="margin-top: 3in;"></third-card>
-        <interactive-map style="margin-top: 290px"></interactive-map>
-        <youtube-video style="margin-top: 300px"></youtube-video>
+        <interactive-map style="margin-top: 2in"></interactive-map>
+        <youtube-video style="margin-top: 2in"></youtube-video>
     </div>
 </template>
 
